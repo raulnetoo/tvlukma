a8d6baaafdb98953825ffef3dca6ce375d80510e
tilukma <143527639+grupolukma@users.noreply.github.com>
2024-04-05 14:02:08 -0300

    anvabril

--- assets/script/aniversariantes.js
+++ assets/script/aniversariantes.js
@@ -2,65 +2,39 @@ const aniversariantes = [
   {
     imagem: 'assets/images/aniversariante/anv01.jpg',
     titulo: 'Aniversariante',
-    nome: 'Gabriela',
-    descricao: '05 de Março',
-    setor: 'Setor: Financeiro'
+    nome: 'Cauê',
+    descricao: '02 de Abril',
+    setor: 'Setor: Expedição'
   },
   {
     imagem: 'assets/images/aniversariante/anv02.jpg',
     titulo: 'Aniversariante',
-    nome: 'Felipe Ferreira',
-    descricao: '11 de Março',
-    setor: 'Setor: Montagem'
+    nome: 'Andreia',
+    descricao: '02 de Abril',
+    setor: 'Setor: DP'
   },
   {
     imagem: 'assets/images/aniversariante/anv03.jpg',
     titulo: 'Aniversariante',
-    nome: 'Gislane',
-    descricao: '11 de Março',
-    setor: 'Setor: Vendas'
+    nome: 'Thaisa',
+    descricao: '07 de Abril',
+    setor: 'Setor: Fiscal'
   },
   {
     imagem: 'assets/images/aniversariante/anv04.jpg',
     titulo: 'Aniversariante',
-    nome: 'Fernanda',
-    descricao: '12 de Março',
-    setor: 'Setor: Vendas'
+    nome: 'Rodrigo Lukasavicus',
+    descricao: '25 de Abril',
+    setor: 'Setor: Diretoria'
   },
   {
-    imagem: 'assets/images/aniversariante/anv05.jpg',
+    imagem: 'assets/images/aniversariante/anv05.png',
     titulo: 'Aniversariante',
-    nome: 'Josué',
-    descricao: '13 de Março',
-    setor: 'Setor: PCP'
+    nome: 'Thiago',
+    descricao: '27 de Abril',
+    setor: 'Setor: Montagem'
   },
-  {
-    imagem: 'assets/images/aniversariante/anv06.jpg',
-    titulo: 'Aniversariante',
-    nome: 'Lukma Recife',
-    descricao: '16 de Março',
-  },
-  {
-    imagem: 'assets/images/aniversariante/anv07.jpg',
-    titulo: 'Aniversariante',
-    nome: 'Carlinhos',
-    descricao: '23 de Março',
-    setor: 'Setor: PCP'
-  },
-  {
-    imagem: 'assets/images/aniversariante/anv08.jpg',
-    titulo: 'Aniversariante',
-    nome: 'Ellen',
-    descricao: '26 de Março',
-    setor: 'Setor: Compras'
-  },
-  {
-    imagem: 'assets/images/aniversariante/anv09.jpg',
-    titulo: 'Aniversariante',
-    nome: 'Marquinhos',
-    descricao: '28 de Março',
-    setor: 'Setor: Expedição'
-  },
+  
   // ... Outras informações dos aniversariantes
 ];
 
