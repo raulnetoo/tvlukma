cea4da4de06dda03920ca1346f906ecf25f66fc2
tilukma <143527639+grupolukma@users.noreply.github.com>
2025-08-01 07:49:57 -0300

    anvagosto2025

--- assets/script/aniversariantes.js
+++ assets/script/aniversariantes.js
@@ -1,56 +1,25 @@
 const aniversariantes = [
   {
-    imagem: 'assets/images/aniversariante/anv07.png',
+    imagem: 'assets/images/aniversariante/anv01.lpg',
     titulo: 'Aniversariante',
-    nome: 'Renato',
-    descricao: '25 de Junho',
-    setor: 'Setor: Supervisor'
-  },
-  {
-    imagem: 'assets/images/aniversariante/anv01.png',
-    titulo: 'Aniversariante',
-    nome: 'Ellen',
-    descricao: '04 de Julho',
-    setor: 'Setor: Vendas'
+    nome: 'Silvana',
+    descricao: '08 de Agosto',
+    setor: 'Setor: Prod. Reles'
   },
   {
     imagem: 'assets/images/aniversariante/anv02.jpg',
     titulo: 'Aniversariante',
-    nome: 'Taianny',
-    descricao: '05 de Julho',
-    setor: 'Setor: Vendas'
-  },
-  {
-    imagem: 'assets/images/aniversariante/anv03.jpg',
-    titulo: 'Aniversariante',
-    nome: 'Larissa',
-    descricao: '06 de Julho',
-    setor: 'Setor: Vendas'
-  },
-  {
-    imagem: 'assets/images/aniversariante/anv04.jpg',
-    titulo: 'Aniversariante',
-    nome: 'Yasmin',
-    descricao: '07 de Julho',
-    setor: 'Setor: Expedição'
-  },
-  {
-    imagem: 'assets/images/aniversariante/anv05.jpg',
-    titulo: 'Aniversariante',
-    nome: 'Marcus',
-    descricao: '08 de Julho',
-    setor: 'Setor: Montagem'
-  },
-  {
-    imagem: 'assets/images/aniversariante/anv06.jpg',
-    titulo: 'Aniversariante',
-    nome: 'Sara',
-    descricao: '10 de Julho',
+    nome: 'Alan',
+    descricao: '13 de Agosto',
     setor: 'Setor: Prod. Reles'
   },
- 
-  
- 
+  {
+    imagem: 'assets/images/aniversariante/anv03.png',
+    titulo: 'Aniversariante',
+    nome: 'Luiz Matos',
+    descricao: '25 de Agosto',
+    setor: 'Setor: Diretoria'
+  },
  
   // ... Outras informações dos aniversariantes
 ];
